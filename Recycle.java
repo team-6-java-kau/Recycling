ca9b3b8abbba7d0e12cd9dae43a8ddb26921e9f8
zahid alfahmi <158223635+zahidaof@users.noreply.github.com>
2024-11-04 21:23:09 +0300

    Update Recycle.java
    fe

--- Recycle.java
+++ Recycle.java
@@ -1,11 +1,67 @@
-public class Recycle{
+public class Recycle {
+    private String itemType;
+    private double size;
+    private String condition;
+    private double itemWeight;
+    private boolean itemRecyclability;
 
-    public void creatlist(int number) {
-        
-        }
+    // Constructor
+    public Recycle(String itemType, double size, String condition, double itemWeight, boolean itemRecyclability) {
+        this.itemType = itemType;
+        this.size = size;
+        this.condition = condition;
+        this.itemWeight = itemWeight;
+        this.itemRecyclability = itemRecyclability;
+    }
 
+    // Getter and Setter methods
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public double getItemWeight() {
+        return itemWeight;
+    }
+
+    public void setItemWeight(double itemWeight) {
+        this.itemWeight = itemWeight;
+    }
+
+    public boolean isItemRecyclability() {
+        return itemRecyclability;
+    }
+
+    public void setItemRecyclability(boolean itemRecyclability) {
+        this.itemRecyclability = itemRecyclability;
+    }
+
+    // Method to create a list of Recycle objects
+    public void createList(int number) {
+        // Implementation for creating a list of recycle items
+    }
+
+    // Method to mark items as done
     public void done(boolean done) {
-        
-        }
-    
+        // Implementation for marking items as done
+    }
 }
