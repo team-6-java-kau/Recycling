22f1001251a062c9748e3e2489d30f108c70acf1
Muu21 <muhapple21@gmail.com>
2024-11-07 20:38:19 +0300

    Adding dis class

--- Main.java
+++ Main.java
@@ -5,24 +5,24 @@ public class Main {
     public static void main(String[] args) {
         List<Recyclableitem> items = Recyclableitem.createList(30);
         Scanner input = new Scanner(System.in);
-        Integer experience_input = 0;
-        experience_input = input.nextInt();
+        Integer experience_input = input.nextInt();
         input.close();
 
-        Employee muhammed = new Employee(1, 5.0, "moha", experience_input);
+        Employee muhammed = new Employee(1, 5.0, "Moha", experience_input);
+        DistributionEmployee distributor = new DistributionEmployee(2, 5.0, "Sara", 3); // Create a distribution employee
 
-        Factory one =new Factory();
-        one.manual(items, muhammed);
+        Factory one = new Factory();
+        one.manual(items, muhammed, distributor); // Pass both employees
+
         if (items != null && !items.isEmpty()) {
             System.out.println("List created successfully with " + items.size() + " items.");
             for (Recyclableitem item : items) {
-                System.out.println(item.getItemType() + " - " +  item.getsortingError() + " - " + item.get_time_to_finish() + " - " + item.getItemWeight());            }
+                System.out.println(item.getItemType() + " - " +  item.getsortingError() + " - " + item.get_time_to_finish() + " - " + item.getItemWeight());
+            }
         } else {
             System.out.println("Failed to create list.");
         }
-       
-        
-        System.out.println(muhammed.errorsNum);
 
+        System.out.println("Errors encountered: " + muhammed.errorsNum);
     }
 }
--- employee.java
+++ employee.java
@@ -24,72 +24,29 @@ public class Employee {
         this.itemsDone = 0;
         this.errorsNum = 0;
     }
+
+    public Integer getEmployeeNumber() { return employeeNumber; }
+    public void setEmployeeNumber(Integer employeeNumber) { this.employeeNumber = employeeNumber; }
+    public Double getWorkingHours() { return workingHours; }
+    public void setWorkingHours(Double workingHours) { this.workingHours = workingHours; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Integer getExperienceYears() { return experienceYears; }
+    public void setExperienceYears(Integer experienceYears) { this.experienceYears = experienceYears; }
+    public Double getTiredness() { return tiredness; }
+    public void setTiredness(Double tiredness) { this.tiredness = tiredness; }
+    public Integer getItemsDone() { return itemsDone; }
+    public void setItemsDone(Integer itemsDone) { this.itemsDone = itemsDone; }
     
-
-    public Integer getEmployeeNumber() {
-        return employeeNumber;
-    }
-
-    public void setEmployeeNumber(Integer employeeNumber) {
-        this.employeeNumber = employeeNumber;
-    }
-
-    public Double getWorkingHours() {
-        return workingHours;
-    }
-
-    public void setWorkingHours(Double workingHours) {
-        this.workingHours = workingHours;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getExperienceYears() {
-        return experienceYears;
-    }
-
-    public void setExperienceYears(Integer experienceYears) {
-        this.experienceYears = experienceYears;
-    }
-
-    public Double getTiredness() {
-        return tiredness;
-    }
-
-    public void setTiredness(Double tiredness) {
-        this.tiredness = tiredness;
-    }
-
-    public Integer getItemsDone() {
-        return itemsDone;
-    }
-
-    public void setItemsDone(Integer itemsDone) {
-        this.itemsDone = itemsDone;
-    }
-
-    public void incrementItemsDone() {
-        this.itemsDone++;
-    }  
-    public void incrementerrorsNum() {
-        this.errorsNum++;
-    }
-
+    public void incrementItemsDone() { this.itemsDone++; }
+    public void incrementerrorsNum() { this.errorsNum++; }
 
     public double calculateSortTime(Recyclableitem recyclable) {
         double baseTime = 2.0;
-    
-        double experienceModifier =Math.abs( 1.0 - (experienceYears * 0.05)); // Up to 20% faster per year
+        double experienceModifier = Math.abs(1.0 - (experienceYears * 0.05)); // Up to 20% faster per year
         double tirednessModifier = 1.0 + (workingHours * 0.01); // Up to 10% slower per hour of work
 
-        
-        double totalModifier = experienceModifier * tirednessModifier ;
+        double totalModifier = experienceModifier * tirednessModifier;
         double sort_time = baseTime * totalModifier;
         recyclable.set_time_to_finish(sort_time);
         return baseTime * totalModifier;
@@ -97,24 +54,21 @@ public class Employee {
 
     public boolean sort(Recyclableitem recyclable) {
         double sortTime = calculateSortTime(recyclable);
-
         workingHours += sortTime;
         tiredness += sortTime * 0.2;
 
         double errorChance = tiredness * 0.05; 
         boolean hasError = Math.random() < errorChance;
 
-    
         recyclable.setisDone_sorting(true);
-
-      
         incrementItemsDone();
-        if (hasError == true){
+        if (hasError) {
             recyclable.setsortingError(true);
         }
         
         return !hasError;
     }
+
     // Method to create a list of Employees with random attributes
     public static List<Employee> createEmployees(int number) {
         List<Employee> employees = new ArrayList<>();
@@ -131,7 +85,4 @@ public class Employee {
 
         return employees;
     }
-
 }
-
-
